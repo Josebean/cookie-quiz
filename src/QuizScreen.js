66d80340885b1794a70264860aa24ff26b63ee1a
Josefina Artigas <josefina@ivyco.eco>
2025-04-03 16:29:44 -0400

    update questions

--- src/QuizScreen.js
+++ src/QuizScreen.js
@@ -1,101 +1,101 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const questions = [
     // Q1
-    {id: 1, question: "How do you handle stressful situations?", 
+    {id: 1, question: "You are a delectable cookie in Kookyville and today’s your day off! What are you doing?", 
         answers: [
-            {id: 1, text: "Stay calm and find a logical solution.", weight: 4},
-            {id: 2, text: "Get overwhelmed but push through.", weight: 3},
-            {id: 3, text: "Seek support from friends or family.", weight: 2},
-            {id: 4, text: "Procrastinate and hope for the best.", weight: 1}
+            {id: 1, text: "Relax by yourself, maybe go to a spa?", weight: 4},
+            {id: 2, text: "Call up some pals :)", weight: 3},
+            {id: 3, text: "Great! Let’s get some chores done!", weight: 2},
+            {id: 4, text: "I have always wanted to take that one class…", weight: 1}
         ]
     },
     // Q2
-    {id: 2, question: "What is your ideal weekend plan?", 
+    {id: 2, question: "You forgot about a last minute errand! On your way to check out, you decide to treat yourself to a snack. Which one do you choose?", 
         answers: [
-            {id: 1, text: "Exploring a new city or going on an adventure.", weight: 4},
-            {id: 2, text: "Staying in with a good book or TV show.", weight: 3},
-            {id: 3, text: "Hanging out with friends at a café or bar.", weight: 2},
-            {id: 4, text: "Working on a personal project or hobby.", weight: 1}
+            {id: 1, text: "Too much candy to choose from T_T", weight: 4},
+            {id: 2, text: "Nothing beats ice cream!", weight: 3},
+            {id: 3, text: "Something chocolate-ly… maybe a cookie…", weight: 2},
+            {id: 4, text: "Obviously a savory chip.", weight: 1}
         ]
     },
     // Q3
-    {id: 3, question: "How do you usually make decisions?", 
+    {id: 3, question: "While marveling at the snacks, you wander into a portal. All of a sudden, you’re in a cookie jar?!", 
         answers: [
-            {id: 1, text: "Based on logic and facts.", weight: 4},
-            {id: 2, text: "By trusting my gut feeling.", weight: 3},
-            {id: 3, text: "I ask for advice from others.", weight: 2},
-            {id: 4, text: "I overthink until the last minute.", weight: 1}
+            {id: 1, text: "Uh-oh, how do I get out of here? I need a plan!", weight: 4},
+            {id: 2, text: "Okay, just need to stay calm. Stay. Calm.", weight: 3},
+            {id: 3, text: "Where am I? A cookie jar? Whoa? What’s happening?", weight: 2},
+            {id: 4, text: "This is amazing! I’m going to have an adventure in this jar!", weight: 1}
         ]
     },
     
     // Q4
-    {id: 4, question: "If you could have any superpower, which one would you choose?", 
+    {id: 4, question: "As you gather your thoughts, you look around and see that there are other cookies around you! What do you do?", 
         answers: [
-            {id: 1, text: "Super intelligence.", weight: 4},
-            {id: 2, text: "Invisibility.", weight: 3},
-            {id: 3, text: "Time travel.", weight: 2},
-            {id: 4, text: "Mind reading.", weight: 1}
+            {id: 1, text: "Talk to them and gather some intel on the situation.", weight: 4},
+            {id: 2, text: "Let’s socialize — new friends!", weight: 3},
+            {id: 3, text: "I don’t want to talk to them… but how else am I going to get out?", weight: 2},
+            {id: 4, text: "Great, there are people here?", weight: 1}
         ]
     },
 
     // Q5
-    {id: 5, question: "What motivates you the most?", 
+    {id: 5, question: "Everyone is welcoming and kind. You find out they also just got here. You start to feel at ease as you talk — who are you the most comfortable with?", 
         answers: [
-            {id: 1, text: "Achieving personal success.", weight: 4},
-            {id: 2, text: "Helping others and making a difference.", weight: 3},
-            {id: 3, text: "Seeking new experiences and excitement.", weight: 2},
-            {id: 4, text: "Finding peace and balance in life.", weight: 1}
+            {id: 1, text: "Charlie Chip, the chocolate chip cookie who is very polite and friendly.", weight: 4},
+            {id: 2, text: "Lucy Zest, the bright, zesty lemon cookie who is lively and veeeery energetic.", weight: 3},
+            {id: 3, text: "Olivia Oates, the crunchy oatmeal cookie that speaks of the simple things in life.", weight: 2},
+            {id: 4, text: "Gia Spice, savory gojuchang cookie who immediately starts discussing the various escape plans they thought up just now.", weight: 1}
         ]
     },
 
     // Q6
-    {id: 4, question: "How do you approach meeting new people?", 
+    {id: 4, question: "The cookies gasp as they look intently around them, through the glass cookie jar. “I think we’re at IKEA!” They suggest exploring, what do you do?", 
         answers: [
-            {id: 1, text: "I love it! I enjoy making new connections.", weight: 4},
-            {id: 2, text: "I am friendly but prefer small groups.", weight: 3},
-            {id: 3, text: "I get nervous but try to be social.", weight: 2},
-            {id: 4, text: "I avoid it when possible.", weight: 1}
+            {id: 1, text: "Let’s relax and stay in the jar, it’s warm and cozy here.", weight: 4},
+            {id: 2, text: "I’m ready to explore! There’s no telling what surprises IKEA might have.", weight: 3},
+            {id: 3, text: "I’ll stay in the jar for now, but I’ll keep an eye on the exits.", weight: 2},
+            {id: 4, text: "Adventure awaits! I’m heading out with a group of cookies right now!", weight: 1}
         ]
     },
 
     // Q7
-    {id: 4, question: "What is your dream job like?", 
+    {id: 4, question: "While wandering through the IKEA aisles, you start getting into window shopping. What catches your eye?", 
         answers: [
-            {id: 1, text: "High-paying and prestigious.", weight: 4},
-            {id: 2, text: "Creative and fulfilling.", weight: 3},
-            {id: 3, text: "Something that allows me to help others.", weight: 2},
-            {id: 4, text: "One that gives me freedom and flexibility.", weight: 1}
+            {id: 1, text: "A cute drink dispenser.", weight: 4},
+            {id: 2, text: "Any light fixture.", weight: 3},
+            {id: 3, text: "Bakeware ofc!", weight: 2},
+            {id: 4, text: "A new rug would be a good touch.", weight: 1}
         ]
     },
 
     // Q8
-    {id: 4, question: "Which animal do you relate to the most?", 
+    {id: 4, question: "Suddenly, you hear tiny footsteps—there’s a determined hungry toddler heading straight toward you! What do you do?", 
         answers: [
-            {id: 1, text: "Lion: Confident and ambitious.", weight: 4},
-            {id: 2, text: "Owl: Wise and introspective.", weight: 3},
-            {id: 3, text: "Dolphin: Social and fun-loving.", weight: 2},
-            {id: 4, text: "Cat: Independent and mysterious.", weight: 1}
+            {id: 1, text: "Panic! I’m too scrumptious, I’ll definitely be spotted!", weight: 4},
+            {id: 2, text: "Stay perfectly still and hope the baby doesn’t see us.", weight: 3},
+            {id: 3, text: "Time to make a run for it! We’ve got to escape before I'm eaten!", weight: 2},
+            {id: 4, text: "I’ll try to distract the baby with some fun tricks, maybe they’ll leave us alone.", weight: 1}
         ]
     },
 
     // Q9
-    {id: 4, question: "What is your biggest fear?", 
+    {id: 4, question: "You’re in full escape mode, but the toddler is still hot on your tail! How do you try to outsmart it?", 
         answers: [
-            {id: 1, text: "Failure.", weight: 4},
-            {id: 2, text: "Loneliness.", weight: 3},
-            {id: 3, text: "The unknown.", weight: 2},
-            {id: 4, text: "Losing control.", weight: 1}
+            {id: 1, text: "I’ll weave through the aisles to lose it in the maze of furniture.", weight: 4},
+            {id: 2, text: "Time to take the high road—climb up to the top of a display and out of reach.", weight: 3},
+            {id: 3, text: "I’m going to confuse the baby with a trail of cookies leading the other way.", weight: 2},
+            {id: 4, text: "Let’s make a bold move—head straight for the exit!", weight: 1}
         ]
     },
 
     // Q10
-    {id: 4, question: "How would your friends describe you?", 
+    {id: 4, question: "You’ve survived the baby chase and found a peaceful corner to relax in. When you open your eyes, you're back home!", 
         answers: [
-            {id: 1, text: "Determined and hardworking.", weight: 4},
-            {id: 2, text: "Thoughtful and kind.", weight: 3},
-            {id: 3, text: "Fun and spontaneous.", weight: 2},
-            {id: 4, text: "Independent and reserved.", weight: 1}
+            {id: 1, text: "Kick back and enjoy some quiet time, maybe even take a nap.", weight: 4},
+            {id: 2, text: "Gather up the cookies and plan the next wild adventure.", weight: 3},
+            {id: 3, text: "Take some time to reflect on the adventure and savor the moment.", weight: 2},
+            {id: 4, text: "Celebrate with a victory dance—it’s been one crazy day!", weight: 1}
         ]
     }
 ];
@@ -109,26 +109,21 @@ function QuizScreen() {
     let weightScore = 0;
 
     function handleAnswer(answer) {
-        console.log("Weight: " + answer.weight);
         selectedAnswer.push(answer.text);
 
-        
-        
+        setScore(prevScore => {
+            const newScore = prevScore + answer.weight;
+            console.log("Updated Score inside setScore: " + newScore);
+            return newScore;
+        });
 
             if (currentQuestion < questions.length - 1) {
-                setCurrentQuestion(currentQuestion + 1);
-                setScore(score + answer.weight);
-                console.log("Score: " + score);
-                console.log("Current Question: " + currentQuestion);
-
-                
+                setCurrentQuestion(prev => prev + 1);
+        
             } else {
                 setQuizFinished(true);
-                setScore(score + answer.weight);
-                console.log(selectedAnswer);
-                console.log("Score: " + score);
-                console.log("Current Question: " + currentQuestion);
 
+                console.log(selectedAnswer);
             }
         }
 
@@ -139,13 +134,16 @@ function QuizScreen() {
         setQuizFinished(false);
     }
     
+    useEffect(() => {
+        console.log("Updated Score (outside handleAnswer):", score);
+    }, [score]);
 
     return (
         <div className="quiz-container">
           {quizFinished ? (
             <div>
               <h2>Quiz Finished!</h2>
-              <p>Your score: </p>
+              <p>Your score: {score}</p>
               <button className="main-button" onClick={restartQuiz}>Restart Quiz</button>
             </div>
           ) : (
